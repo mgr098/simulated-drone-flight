2d7536083b47444e50b85cf73064bfe54fdcf3ea
mgr098 <mgr098@uit.no>
2022-06-20 11:07:24 +0200

    move around and take photos

--- src/sim/photo.py
+++ src/sim/photo.py
@@ -19,21 +19,29 @@ from olympe.messages.camera import (
     photo_progress,
 )
 
+#TODO:
+# Restructure code
+# Fly while taking photo ascync
+# Fly without stopping
+# Fly without stopping while taking photos
+# Fly while streaming, look into livestream
+# Fly and use different cameras (at the same time? while streaming?)
+
+TIMEOUT = 120
 DRONE_IP = os.environ.get("DRONE_IP", "10.202.0.1")
 DRONE_MEDIA_PORT = os.environ.get("DRONE_MEDIA_PORT", "80")
-TIMEOUT = 120
-
 
 olympe.log.update_config({"loggers": {"olympe": {"level": "INFO"}}})
 
 def main():
     drone = olympe.Drone(DRONE_IP, media_port=DRONE_MEDIA_PORT)
     drone.connect(retry=3)
-    drone(set_camera_mode(cam_id=0, value="photo")).wait()
     fly(drone)
     drone.disconnect()
 
 def fly(drone):
+        drone(set_camera_mode(cam_id=0, value="photo")).wait()
+        
         # Takeoff, fly, land, ...
         print("Takeoff if necessary...")
         drone(
@@ -65,30 +73,22 @@ def fly(drone):
         drone.media.download_dir = tempfile.mkdtemp(prefix="olympe_photo_example")
         drone(photo_progress(result="photo_saved", _policy="wait"))
 
+        drone(MaxTilt(30)).wait().success()
+        drone(extended_move_by(10, -30, -10, 0, 200, 200, 20,_timeout=120)).wait().success()
         drone(take_photo(cam_id=0)).wait()
-        drone(moveBy(2, 0, 0, 0, _timeout=TIMEOUT)).wait().success()
+        drone(extended_move_by(30, -5, -20, 0,200, 200, 20,_timeout=120)).wait().success()
+        drone(take_photo(cam_id=0)).wait()
+        drone(extended_move_by(30, 0, -5, 0,200, 200, 20,_timeout=120)).wait().success()
+        drone(take_photo(cam_id=0)).wait()
+        drone(extended_move_by(31, 21, 20, 0, 200, 200, 20,_timeout=120)).wait().success()
+        drone(take_photo(cam_id=0)).wait()
+        drone(Landing()).wait().success()
         drone(take_photo(cam_id=0)).wait()
 
         media_list = drone.media.list_media()
-        print("@" + str(media_list))
 
         for media in media_list:
             drone(download_media(media, integrity_check=True)).wait()
-
-
-
-        # >> Landing()
-        # print("Moving 1/4")
-        # drone(MaxTilt(30)).wait().success()
-        # drone(extended_move_by(10, -30, -10, 0, 200, 200, 20,_timeout=120)).wait().success()
-        # print("Moving 2/4")
-        # drone(extended_move_by(30, -5, -20, 0,200, 200, 20,_timeout=120)).wait().success()
-        # print("Moving 3/4")
-        # drone(extended_move_by(30, 0, -5, 0,200, 200, 20,_timeout=120)).wait().success()
-        # print("Moving 3/4")
-        # drone(extended_move_by(31, 21, 20, 0, 200, 200, 20,_timeout=120)).wait().success()
-        # print("Landing...")
-        # print("Landed\n")
 
 if __name__ == "__main__":
     main()
